ed077380f980102744c6e4c88e4cdc73fcd6050c
Daniel Lesio <lesiewiczsales@gmail.com>
2020-03-15 20:02:44 +0100

    final

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 const gulp = require('gulp');
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer")
+const autoprefixer = require("autoprefixer");
+	
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const uglify = require("gulp-uglify");
+
 const browserSync = require("browser-sync").create();
-
-
+const paths = {
+    source : './scripts',
+    build : "./js"
+}
+function scripts(){
+    return (
+        browserify({
+            entries : [`${paths.source}/index.js`],
+            transform : [babelify.configure({presets : ["@babel/preset-env"]})]
+        })
+        .bundle()
+        .pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest(`${paths.build}/`))
+    )
+  }
 function style(){
     const plugins = [
         autoprefixer({
@@ -18,6 +40,7 @@ function style(){
     .pipe(browserSync.stream())
 }
 
+
 function watch(){
     browserSync.init({
         server : {
@@ -26,8 +49,8 @@ function watch(){
     });
     gulp.watch('./scss/**/*.scss',style);
     gulp.watch("./*.html").on("change",browserSync.reload);
-
+    gulp.watch("./scripts/**/*.js",scripts).on("change",browserSync.reload);
 }
-
-exports.watch = watch
+exports.scripts = scripts;
+exports.watch = watch;
 exports.style = style;
---
+++ scripts/index.js
@@ -0,0 +1,2 @@
+import { test } from './scripts/app'
+test();
